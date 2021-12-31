91174c69edaf9aae38ed570b94e858f9523c9d11
MIkaela Braga <bragamikaela@gmail.com>
2021-12-31 03:08:20 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const PORT = '3000';
 
 const talkerFile = 'talker.json';
 
+// requisito 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const dataTalkerFile = await fs.readFile(talkerFile, 'utf8');
+  const dataTalkerFileJSON = JSON.parse(dataTalkerFile);
+
+  const verifySpeakerId = dataTalkerFileJSON.find((speaker) => speaker.id === parseInt(id, 10));
+
+  if (!verifySpeakerId) {
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(200).json(verifySpeakerId);
+});
+
+// requisito 1
 app.get('/talker', async (_req, res, _next) => {
   const dataTalkerFile = await fs.readFile(talkerFile, 'utf8');
   const dataTalkerFileJSON = JSON.parse(dataTalkerFile);
